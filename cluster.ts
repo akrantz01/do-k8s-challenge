10e6940f771c9c9d3f3af3c244f6bb4278c76d1c
Alex Krantz <alex@krantz.dev>
2021-12-22 19:26:04 -0800

    Add code formatting

--- cluster.ts
+++ cluster.ts
@@ -1,5 +1,9 @@
-import {ComponentResource, Inputs, ResourceOptions} from "@pulumi/pulumi";
-import {KubernetesCluster, Vpc, getKubernetesVersions} from "@pulumi/digitalocean";
+import { ComponentResource, Inputs, ResourceOptions } from '@pulumi/pulumi';
+import {
+  KubernetesCluster,
+  Vpc,
+  getKubernetesVersions,
+} from '@pulumi/digitalocean';
 
 export interface Args {
   region: string;
@@ -12,31 +16,42 @@ export class Cluster extends ComponentResource {
 
   constructor(name: string, args: Args, opts?: ResourceOptions) {
     const inputs: Inputs = { options: opts };
-    super("akrantz01:do-k8s-challenge:Cluster", name, inputs, opts);
+    super('akrantz01:do-k8s-challenge:Cluster', name, inputs, opts);
 
     // Automatically connect created resources with this module
     const defaultResourceOptions: ResourceOptions = { parent: this };
 
     // Create a new VPC for all the networking
-    this.vpc = new Vpc(`${name}-vpc`, {region: args.region}, defaultResourceOptions);
+    this.vpc = new Vpc(
+      `${name}-vpc`,
+      { region: args.region },
+      defaultResourceOptions,
+    );
 
     // Create the cluster with the default version
-    const latestVersion = getKubernetesVersions({}, defaultResourceOptions).then(v => v.latestVersion);
-    this.k8s = new KubernetesCluster(`${name}-k8s`, {
-      version: latestVersion,
-      region: args.region,
-      ha: false,
-      autoUpgrade: true,
-      vpcUuid: this.vpc.id,
-      maintenancePolicy: {
-        startTime: "00:00",
-        day: "sunday",
+    const latestVersion = getKubernetesVersions(
+      {},
+      defaultResourceOptions,
+    ).then((v) => v.latestVersion);
+    this.k8s = new KubernetesCluster(
+      `${name}-k8s`,
+      {
+        version: latestVersion,
+        region: args.region,
+        ha: false,
+        autoUpgrade: true,
+        vpcUuid: this.vpc.id,
+        maintenancePolicy: {
+          startTime: '00:00',
+          day: 'sunday',
+        },
+        nodePool: {
+          name: `${name}-node-pool`,
+          size: args.nodeType,
+          nodeCount: 3,
+        },
       },
-      nodePool: {
-        name: `${name}-node-pool`,
-        size: args.nodeType,
-        nodeCount: 3,
-      }
-    }, defaultResourceOptions)
+      defaultResourceOptions,
+    );
   }
 }
--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
-import {Config} from "@pulumi/pulumi";
-import {Cluster} from "./cluster";
+import { Config } from '@pulumi/pulumi';
+import { Cluster } from './cluster';
 
 // Pull the configuration values
 const config = new Config();
-const nodeType = config.get("nodeType") || "s-2vcpu-4gb";
-const region = config.require("region");
+const nodeType = config.get('nodeType') || 's-2vcpu-4gb';
+const region = config.require('region');
 
 // Create the cluster with its configuration
-const cluster = new Cluster("cluster", {nodeType, region});
-const kubeconfig = cluster.k8s.kubeConfigs.apply(configs => configs[0].rawConfig);
+const cluster = new Cluster('cluster', { nodeType, region });
+const kubeconfig = cluster.k8s.kubeConfigs.apply(
+  (configs) => configs[0].rawConfig,
+);
