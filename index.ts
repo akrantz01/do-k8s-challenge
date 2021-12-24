02b0eaa71b92177282104c4d9ac44cd8b22b1a75
Alex Krantz <alex@krantz.dev>
2021-12-24 01:14:30 -0800

    Test linkerd integration worked properly

--- index.ts
+++ index.ts
@@ -4,6 +4,9 @@ import { Config } from '@pulumi/pulumi';
 import { Ambassador } from './ambassador';
 import { Cluster } from './cluster';
 import { Linkerd } from './linkerd';
+import { Namespace, Service } from '@pulumi/kubernetes/core/v1';
+import { Deployment } from '@pulumi/kubernetes/apps/v1';
+import { Mapping } from './crds/getambassador/v3alpha1';
 
 // Pull the configuration values
 const config = new Config();
@@ -30,4 +33,120 @@ const ambassador = new Ambassador(
   'ambassador',
   { version: '2.1.0', linkerd: true },
   { ...kubernetesOpts, dependsOn: linkerd.ready },
+);
+
+// Test injection was successful
+const namespace = new Namespace(
+  'testing-namespace',
+  {
+    metadata: {
+      name: 'default',
+      annotations: {
+        'linkerd.io/inject': 'enabled',
+      },
+    },
+  },
+  { ...kubernetesOpts, dependsOn: [ambassador] },
+);
+
+const deployment = new Deployment(
+  'testing-deployment',
+  {
+    metadata: {
+      name: 'qotm',
+      namespace: 'default',
+    },
+    spec: {
+      replicas: 1,
+      selector: {
+        matchLabels: {
+          app: 'qotm',
+        },
+      },
+      template: {
+        metadata: {
+          labels: {
+            app: 'qotm',
+          },
+        },
+        spec: {
+          containers: [
+            {
+              name: 'qotm',
+              image: 'docker.io/datawire/qotm:1.7',
+              ports: [
+                {
+                  name: 'http-api',
+                  containerPort: 5000,
+                },
+              ],
+              env: [
+                {
+                  name: 'POD_IP',
+                  valueFrom: {
+                    fieldRef: {
+                      fieldPath: 'status.podIP',
+                    },
+                  },
+                },
+              ],
+              readinessProbe: {
+                httpGet: {
+                  path: '/health',
+                  port: 5000,
+                },
+                initialDelaySeconds: 60,
+                periodSeconds: 3,
+              },
+              resources: {
+                limits: {
+                  cpu: '0.1',
+                  memory: '100Mi',
+                },
+              },
+            },
+          ],
+        },
+      },
+    },
+  },
+  { ...kubernetesOpts, dependsOn: [namespace] },
+);
+
+const service = new Service(
+  'testing-service',
+  {
+    metadata: {
+      name: 'qotm-linkerd2',
+      namespace: 'default',
+    },
+    spec: {
+      ports: [
+        {
+          name: 'http',
+          port: 80,
+          targetPort: 5000,
+        },
+      ],
+      selector: {
+        app: 'qotm',
+      },
+    },
+  },
+  { ...kubernetesOpts, dependsOn: [deployment] },
+);
+
+new Mapping(
+  'testing-mapping',
+  {
+    metadata: {
+      name: 'linkerd2-qotm',
+    },
+    spec: {
+      hostname: '*',
+      prefix: '/qotm-linkerd2/',
+      service: 'qotm-linkerd2',
+    },
+  },
+  { ...kubernetesOpts, dependsOn: [service] },
 );
